7ec29250507577edae2abadcf7e59ea512abee5d
Pekarn X <94284783+ShadowX2th@users.noreply.github.com>
2023-11-14 18:31:22 +0700

    Update CUSHUB.lua

--- CUSHUB.lua
+++ CUSHUB.lua
@@ -5298,20 +5298,6 @@ function GetBladeHit()
     end
     return weapon
 end
-function AttackHit()
-    local CombatFrameworkLib = debug.getupvalues(require(game:GetService("Players").LocalPlayer.PlayerScripts.CombatFramework))
-    local CmrFwLib = CombatFrameworkLib[2]
-    local plr = game.Players.LocalPlayer
-    for i = 1, 1 do
-        local bladehit = require(game.ReplicatedStorage.CombatFramework.RigLib).getBladeHits(plr.Character,{plr.Character.HumanoidRootPart},60)
-        local cac = {}
-        local hash = {}
-        for k, v in pairs(bladehit) do
-            if v.Parent:FindFirstChild("HumanoidRootPart") and not hash[v.Parent] then
-                table.insert(cac, v.Parent.HumanoidRootPart)
-                hash[v.Parent] = true
-            end
-        end
         bladehit = cac
         if #bladehit > 0 then
             pcall(function()
